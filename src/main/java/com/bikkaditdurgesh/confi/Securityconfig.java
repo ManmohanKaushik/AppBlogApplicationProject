fd463fdecdc1f4747a4515da1db7792ff894cdb1
SIYARAM <manmohankaushik76035@gmail.com>
2023-12-24 18:36:06 +0530

    My durgesh project commit

--- src/main/java/com/bikkaditdurgesh/confi/Securityconfig.java
+++ src/main/java/com/bikkaditdurgesh/confi/Securityconfig.java
@@ -5,7 +5,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -14,6 +16,8 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.DefaultSecurityFilterChain;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
@@ -25,47 +29,73 @@ import com.bikkaditdurgesh.security.JwtAuthenticationFilter;
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 @EnableWebMvc
-public class Securityconfig extends WebSecurityConfigurerAdapter {
-	public static final String[] PUBLIC_URLS = { "/api/v1/auth/**", "/v3/api-docs", "/v2/api-docs",
-			"/swagger-resources/**", "/swagger-ui/**", "/webjars/**"
+public class Securityconfig {
+    public static final String[] PUBLIC_URLS = {"/api/v1/auth/**", "/v3/api-docs", "/v2/api-docs",
+            "/swagger-resources/**", "/swagger-ui/**", "/webjars/**"
 
-	};
+    };
 
-	@Autowired
-	private CustomUserDetailService customUserDetailService;
-	@Autowired
-	private JwtAuthenticationEnteryPoint jwtAuthenticationEnteryPoint;
-	@Autowired
-	private JwtAuthenticationFilter jwtAuthenticationFilter;
+    @Autowired
+    private CustomUserDetailService customUserDetailService;
+    @Autowired
+    private JwtAuthenticationEnteryPoint jwtAuthenticationEnteryPoint;
+    @Autowired
+    private JwtAuthenticationFilter jwtAuthenticationFilter;
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.csrf().disable().authorizeHttpRequests()
-				// .antMatchers("/api/v1/auth/**").permitAll()
-				// .antMatchers("/v3/api-docs").permitAll()
-				.antMatchers(PUBLIC_URLS).permitAll().antMatchers(HttpMethod.GET).permitAll().anyRequest()
-				.authenticated().and().exceptionHandling().authenticationEntryPoint(this.jwtAuthenticationEnteryPoint)
-				.and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    /*@Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeHttpRequests()
+                // .antMatchers("/api/v1/auth/**").permitAll()
+                // .antMatchers("/v3/api-docs").permitAll()
+                .antMatchers(PUBLIC_URLS).permitAll().antMatchers(HttpMethod.GET).permitAll().anyRequest()
+                .authenticated().and().exceptionHandling().authenticationEntryPoint(this.jwtAuthenticationEnteryPoint)
+                .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
-		http.addFilterBefore(this.jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+        http.addFilterBefore(this.jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
-	}
+    }*/
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable().authorizeHttpRequests()
+                // .antMatchers("/api/v1/auth/**").permitAll()
+                // .antMatchers("/v3/api-docs").permitAll()
+                .antMatchers(PUBLIC_URLS).permitAll().antMatchers(HttpMethod.GET).permitAll().anyRequest()
+                .authenticated().and().exceptionHandling().authenticationEntryPoint(this.jwtAuthenticationEnteryPoint)
+                .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
-	@Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(this.customUserDetailService).passwordEncoder(passwordEncoder());
-	}
+        http.addFilterBefore(this.jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+        http.authenticationProvider(daoAuthenticationProvider());
+        DefaultSecurityFilterChain build = http.build();
 
-	@Bean
-	public PasswordEncoder passwordEncoder() {
-		return new BCryptPasswordEncoder();
+        return build;
+    }
 
-	}
+    /*@Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(this.customUserDetailService).passwordEncoder(passwordEncoder());
+    }*/
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setUserDetailsService(this.customUserDetailService);
+        provider.setPasswordEncoder(passwordEncoder());
+        return provider;
+    }
 
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+
+    }
+/*
 	@Bean
 	@Override
 	public AuthenticationManager authenticationManagerBean() throws Exception {
 		return super.authenticationManagerBean();
-	}
+	}*/
 
+    @Bean
+    public AuthenticationManager authenticationManagerBean(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
 }
